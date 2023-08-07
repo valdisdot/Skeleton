50e9ec2be864872f5c35477f1628d5034c5c3f54
valdisdot <valdisdot@gmail.com>
2023-08-07 22:55:27 +0300

    Debug: add method runGetOnTheDesk

--- src/main/java/com/valdisdot/util/tool/Debug.java
+++ src/main/java/com/valdisdot/util/tool/Debug.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Debug {
+    public static void runGetOnTheDesk(JComponent component, int startWithRGBHex){
+        new Thread(getOnTheDesk(component, startWithRGBHex)).start();
+    }
+
     public static Runnable getOnTheDesk(JComponent component, int startWithRGBHex) {
         return () -> {
             JPanel panel = new JPanel();
@@ -14,33 +18,24 @@ public class Debug {
             );
             panel.setPreferredSize(size);
 
-            //gpt
-            int color = startWithRGBHex; //hex
-            panel.setBackground(new Color(color));
-            int red = (color >> 16) & 0xFF;
-            int green = (color >> 8) & 0xFF;
-            int blue = color & 0xFF;
+            panel.setBackground(new Color(startWithRGBHex));
+            int red = (startWithRGBHex >> 16) & 0xFF;
+            int green = (startWithRGBHex >> 8) & 0xFF;
+            int blue = startWithRGBHex & 0xFF;
             int maxColorValue = 255;
 
             int reversedRed = maxColorValue - red;
             int reversedGreen = maxColorValue - green;
             int reversedBlue = maxColorValue - blue;
-
-            // Reconstruct the new reversed RGB color
             int reversedColor = (reversedRed << 16) | (reversedGreen << 8) | reversedBlue;
             component.setBackground(new Color(reversedColor));
 
             panel.add(component);
-            JFrame frame = new JFrame("DECK | com.valdisdot.util");
+            JFrame frame = new JFrame("DESK | com.valdisdot.util");
             frame.add(panel);
             frame.repaint();
             frame.pack();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        new JFrame(){
-//            {
-//                t
-//            }
-//        };
             frame.setVisible(true);
         };
     }
