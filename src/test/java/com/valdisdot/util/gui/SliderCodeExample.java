42b8f7d256f1051f69df6ca1c8243c8fb9c230dc
valdisdot <valdisdot@gmail.com>
2023-08-07 22:58:50 +0300

    create classes SpinnerCodeExample, SliderCodeExample

---
+++ src/test/java/com/valdisdot/util/gui/SliderCodeExample.java
@@ -0,0 +1,9 @@
+package com.valdisdot.util.gui;
+
+import com.valdisdot.util.ui.gui.element.Slider;
+
+public class SliderCodeExample {
+    public static void main(String[] args) {
+        new Slider();
+    }
+}
---
+++ src/test/java/com/valdisdot/util/gui/SpinnerCodeExample.java
@@ -0,0 +1,9 @@
+package com.valdisdot.util.gui;
+
+import com.valdisdot.util.ui.gui.element.Spinner;
+
+public class SpinnerCodeExample {
+    public static void main(String[] args) {
+        new Spinner<Integer>().getDataCell();
+    }
+}
