a9b5fce121da35c22828885898198fc83359f734
valdisdot <valdisdot@gmail.com>
2023-08-07 22:30:22 +0300

    create class Spinner

--- src/main/java/com/valdisdot/util/ui/gui/element/Spinner.java
+++ src/main/java/com/valdisdot/util/ui/gui/element/Spinner.java
@@ -1,5 +1,26 @@
 package com.valdisdot.util.ui.gui.element;
 
-public class Spinner {
+import com.valdisdot.util.tool.Debug;
+
+import javax.swing.*;
+
+public class Spinner<T extends Integer> extends AbstractElement<T> {
+    @Override
+    protected boolean pleaseAcceptThatYouHaveDone() {return false;}
+
+    public Spinner() {
+        JSpinner visibleSpinner = new JSpinner();
+        getOnDesk(visibleSpinner);
+    }
+
+    //remove method later
+    public static void getOnDesk(JComponent component) {
+        //removed
+        Debug.getOnTheDesk(component, 7662253);
+    }
+
     //for JSpinner, later
+    protected Integer value() {
+        return T.valueOf(0);
+    }
 }
