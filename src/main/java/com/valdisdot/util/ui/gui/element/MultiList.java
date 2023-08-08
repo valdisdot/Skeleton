ad20ab59b27895485395525574d5d3ffbf684754
valdisdot <valdisdot@gmail.com>
2023-08-08 20:55:31 +0300

    MultiList: fill with logic
    - create two constructors

--- src/main/java/com/valdisdot/util/ui/gui/element/MultiList.java
+++ src/main/java/com/valdisdot/util/ui/gui/element/MultiList.java
@@ -1,5 +1,27 @@
 package com.valdisdot.util.ui.gui.element;
 
-public class MultiList {
-    //for JList, later
+import com.valdisdot.util.data.DataCell;
+
+import javax.swing.*;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Vector;
+
+public class MultiList<D> extends JElement<Collection<D>> {
+    public MultiList(String name, JList<D> list) {
+        list.setName(Objects.requireNonNull(name));
+        completeInitialization(list, new DataCell<>(
+                list::getSelectedValuesList,
+                (val) -> list.clearSelection()
+        ));
+    }
+
+    public MultiList(String name, Collection<D> items) {
+        this(name, new JList<>(new Vector<>(Objects.requireNonNull(items))));
+    }
+
+    @Override
+    protected boolean pleaseAcceptThatYouHaveDone() {
+        return true;
+    }
 }
