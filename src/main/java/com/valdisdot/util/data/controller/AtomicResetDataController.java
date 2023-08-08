7ed264704574503c3689aa1e79bfe13da68e116d
valdisdot <valdisdot@gmail.com>
2023-08-08 14:45:11 +0300

    AtomicResetDataController: add comment

--- src/main/java/com/valdisdot/util/data/controller/AtomicResetDataController.java
+++ src/main/java/com/valdisdot/util/data/controller/AtomicResetDataController.java
@@ -5,6 +5,12 @@ import com.valdisdot.util.data.element.DataCellGroup;
 import java.util.Objects;
 import java.util.function.Function;
 
+/**
+ * implementation for atomic reset of DataCells
+ * for example, we have some DataCell with name "data_cell"
+ * 1) we want to set data on this cell to "default" -> the Function<String, D> resetFunction has to accept DataCell name "data_cell" * and return "default"
+ * 2)we have Iterable ["val1", "val2"] and want to set data on the cell to iterable.next -> *~ and return iterable.next
+ */
 public class AtomicResetDataController<D> implements DataController {
     private final DataCellGroup<D> dataCellGroup;
     private final Function<String, D> resetFunction;
