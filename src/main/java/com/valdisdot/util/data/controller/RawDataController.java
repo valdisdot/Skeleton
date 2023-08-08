b34683547d23de0153f8eff947521a2e260beb23
valdisdot <valdisdot@gmail.com>
2023-08-08 15:18:42 +0300

    RawDataController: add comment

--- src/main/java/com/valdisdot/util/data/controller/RawDataController.java
+++ src/main/java/com/valdisdot/util/data/controller/RawDataController.java
@@ -6,6 +6,10 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
 
+/*
+* implementation that reads and consumes the Map of raw data from DataCells
+* Code example: com.valdisdot.util.data.DataControllerCodeExample
+* */
 public class RawDataController<D> implements DataController {
     private final DataCellGroup<D> dataCellGroup;
     private final Consumer<Map<String, D>> dataConsumer;
