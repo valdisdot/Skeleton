9615d62a438a9a0a9c193cc5fb9433261baa64ec
valdisdot <valdisdot@gmail.com>
2023-08-08 13:14:36 +0300

    Panel: modify
    - charge logic from the adding to the list to adding directly in the JPanel instance
    - add specified constructors
    - remove method 'addFirst'
    - fix Collection generic type for method "add"

--- src/main/java/com/valdisdot/util/ui/gui/component/Panel.java
+++ src/main/java/com/valdisdot/util/ui/gui/component/Panel.java
@@ -1,34 +1,57 @@
 package com.valdisdot.util.ui.gui.component;
 
+import com.valdisdot.util.ui.gui.util.ContentPanel;
+import com.valdisdot.util.ui.gui.util.LayoutManager;
+
 import javax.swing.*;
 import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Objects;
 import java.util.function.Supplier;
 
 public class Panel implements Supplier<JComponent> {
-    private final List<Supplier<JComponent>> queue;
+    private final JPanel contentPanel;
+
+    public Panel(JPanel contentPanel) {
+        this.contentPanel = Objects.requireNonNull(contentPanel);
+    }
 
     public Panel() {
-        queue = new LinkedList<>();
+        this(new ContentPanel());
+    }
+
+    public Panel(LayoutManager.Order order, int indentX, int indentY, boolean isInternalPanel) {
+        this(new ContentPanel(Objects.requireNonNull(order), indentX, indentY, isInternalPanel));
+    }
+
+    public Panel(Collection<? extends Supplier<JComponent>> componentSuppliers, JPanel contentPanel) {
+        this.contentPanel = Objects.requireNonNull(contentPanel);
+        this.add(componentSuppliers);
+    }
+
+    public Panel(Collection<? extends Supplier<JComponent>> componentSuppliers) {
+        this(
+                componentSuppliers,
+                new ContentPanel()
+        );
+    }
+
+    public Panel(Collection<? extends Supplier<JComponent>> componentSuppliers, LayoutManager.Order order, int indentX, int indentY, boolean isInternalPanel) {
+        this(
+                componentSuppliers,
+                new ContentPanel(Objects.requireNonNull(order), indentX, indentY, isInternalPanel)
+        );
     }
 
     public void add(Supplier<JComponent> componentSupplier) {
-        queue.add(Objects.requireNonNull(componentSupplier));
+        contentPanel.add(Objects.requireNonNull(componentSupplier).get());
     }
 
-    public void add(Collection<Supplier<JComponent>> collection) {
-        queue.addAll(Objects.requireNonNull(collection));
-    }
-
-    public void addFirst(Supplier<JComponent> componentSupplier) {
-        queue.add(0, Objects.requireNonNull(componentSupplier));
+    public void add(Collection<? extends Supplier<JComponent>> collection) {
+        Objects.requireNonNull(collection).forEach(componentSupplier -> contentPanel.add(componentSupplier.get()));
     }
 
     @Override
     public JComponent get() {
-        return null;
+        return contentPanel;
     }
-    //later
 }
