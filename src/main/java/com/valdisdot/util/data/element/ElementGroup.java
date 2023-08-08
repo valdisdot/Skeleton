37961f0af3775b783389b6a7abc22a2b181dc6f8
valdisdot <valdisdot@gmail.com>
2023-08-08 12:57:18 +0300

    ElementGroup: modify
    - fix generic type for the Collection from the constructor
    - remove restriction implementation method put()

--- src/main/java/com/valdisdot/util/data/element/ElementGroup.java
+++ src/main/java/com/valdisdot/util/data/element/ElementGroup.java
@@ -8,16 +8,13 @@ import java.util.stream.Collectors;
 
 //implementation with boilerplate for Element. Code example: in com.valdisdot.util.gui.DataCellCodeExample
 public class ElementGroup<D> extends Naming<D> {
-    public ElementGroup(Collection<Element<D, ?>> elements) {
+    public ElementGroup(Collection<? extends Element<D, ?>> elements) {
         super(elements.stream().collect(Collectors.toMap(Element::getName, Element::getDataCell)));
     }
 
-    public void addDataCell(Element<D, ?> element) {
+    public void addElement(Element<D, ?> element) {
         put(element.getName(), element.getDataCell());
     }
 
-    @Override
-    public void put(String dataCellName, DataCell<D> dataCell) {
-        throw new UnsupportedOperationException("Adding a data cell is not supported in ElementGroup");
-    }
+
 }
