e22d9729a5495f7510858e2f729cabb2029aa4cc
valdisdot <valdisdot@gmail.com>
2023-08-08 15:18:22 +0300

    BulkResetDataController: add comment

--- src/main/java/com/valdisdot/util/data/controller/BulkResetDataController.java
+++ src/main/java/com/valdisdot/util/data/controller/BulkResetDataController.java
@@ -3,7 +3,10 @@ package com.valdisdot.util.data.controller;
 import com.valdisdot.util.data.element.DataCellGroup;
 
 import java.util.Objects;
-
+/*
+* implementation for bulk reset of all DataCells
+* for example, we need set all data in DataCells to "" or is the set function of the DataCell works like a switcher and ignores the resetValue
+* */
 public class BulkResetDataController<D> implements DataController {
     private final DataCellGroup<D> dataCellGroup;
     private final D resetValue;
