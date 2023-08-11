2b86cea4ef6fcb678660c642998fe243aeac1eac
valdisdot <valdisdot@gmail.com>
2023-08-11 22:56:53 +0300

    ContentButton: add change color logic when is pressed

--- src/main/java/com/valdisdot/util/ui/gui/element/ContentButton.java
+++ src/main/java/com/valdisdot/util/ui/gui/element/ContentButton.java
@@ -1,8 +1,10 @@
 package com.valdisdot.util.ui.gui.element;
 
 import com.valdisdot.util.data.DataCell;
+import com.valdisdot.util.tool.Colors;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.util.Collection;
 import java.util.Objects;
@@ -11,13 +13,21 @@ import java.util.function.Supplier;
 
 //the class uses the same logic as for CheckBox class
 public class ContentButton extends JElement<String> {
-    private boolean wasPressed;
+    private boolean wasPressed = false;
 
     public ContentButton(String name, JButton button, String valueIfWasPressed, String valueIfWasNotPressed) {
         button.setName(name);
+        button.setFocusable(false);
+        Color original = button.getBackground();
+        Color clicked = new Color(Colors.isDark(original.getRGB()) ? original.getRGB() + 128 : original.getRGB() - 64);
+        button.addActionListener(l -> {
+            wasPressed = !wasPressed;
+            if(wasPressed) button.setBackground(clicked);
+            else button.setBackground(original);
+        });
+
         Supplier<String> supplierFunction = () -> wasPressed ? valueIfWasPressed : valueIfWasNotPressed;
         Consumer<String> consumerFunction = (value) -> wasPressed = !wasPressed;
-        button.addActionListener(l -> wasPressed = !wasPressed);
         completeInitialization(
                 button,
                 new DataCell<>(supplierFunction, consumerFunction)
