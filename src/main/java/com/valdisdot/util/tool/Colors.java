343c3b175103f5f550742e65eb9241d6729bb826
valdisdot <valdisdot@gmail.com>
2023-08-11 23:31:42 +0300

    create class Colors
    - class contains utilities/parsers and convertors for working with colors

---
+++ src/main/java/com/valdisdot/util/tool/Colors.java
@@ -0,0 +1,29 @@
+package com.valdisdot.util.tool;
+
+//util class for colors
+public class Colors {
+    //for analysing the brightness on a color
+    public static int getLuma(int hexColor) {
+        int r = (hexColor >> 16) & 0xff;
+        int g = (hexColor >> 8) & 0xff;
+        int b = (hexColor >> 0) & 0xff;
+        int luma = (int) (0.2126 * r + 0.7152 * g + 0.0722 * b); //ITU-R BT.709
+        return luma;
+    }
+
+    public static boolean isDark(int hexColor) {
+        return getLuma(hexColor) < 128;
+    }
+
+    public static int reverse(int hexColor) {
+        int red = (hexColor >> 16) & 0xFF;
+        int green = (hexColor >> 8) & 0xFF;
+        int blue = hexColor & 0xFF;
+        int maxColorValue = 255;
+
+        int reversedRed = maxColorValue - red;
+        int reversedGreen = maxColorValue - green;
+        int reversedBlue = maxColorValue - blue;
+        return (reversedRed << 16) | (reversedGreen << 8) | reversedBlue;
+    }
+}
