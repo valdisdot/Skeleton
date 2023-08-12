cdd44b7378fd44df62a4765227e4c98c58a1c8b0
valdisdot <valdisdot@gmail.com>
2023-08-12 09:55:08 +0300

    create class RadioButtonsExperiment

---
+++ src/test/java/com/valdisdot/util/gui/RadioButtonsExperiment.java
@@ -0,0 +1,26 @@
+package com.valdisdot.util.gui;
+
+import com.valdisdot.util.tool.FrameFactory;
+import com.valdisdot.util.tool.ValuesParser;
+import com.valdisdot.util.ui.gui.element.JElement;
+import com.valdisdot.util.ui.gui.element.RadioButtons;
+
+import javax.swing.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class RadioButtonsExperiment {
+    public static void main(String[] args) {
+        experiment1();
+    }
+
+    static void experiment1() {
+        JElement<String> radio = new RadioButtons(
+                "fruit",
+                new LinkedHashMap<>(Map.of(
+                        new JRadioButton("Apple"), "apple",
+                        new JRadioButton("Orange"), "orange"
+                )));
+        FrameFactory.playOnDesk(radio, "", map -> System.out.println(ValuesParser.toKeySemicolonValueString(map)));
+    }
+}
