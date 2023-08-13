38b5b872740a5865e49c98501a81a5073577b684
valdisdot <valdisdot@gmail.com>
2023-08-13 23:10:23 +0300

    ScrollableTextArea: update decoration for JScrollPane (as for MultiList)

--- src/main/java/com/valdisdot/util/ui/gui/element/ScrollableTextArea.java
+++ src/main/java/com/valdisdot/util/ui/gui/element/ScrollableTextArea.java
@@ -1,6 +1,10 @@
 package com.valdisdot.util.ui.gui.element;
 
+import com.valdisdot.util.ui.gui.tool.Colors;
+
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+import java.awt.*;
 
 //class wraps a JTextArea into a JScrollPane
 //it is useful when user inputs a lot of text into text area -> auto scrolling
@@ -9,7 +13,34 @@ public class ScrollableTextArea extends TextInputElement {
 
     public ScrollableTextArea(String name, JTextArea textField) {
         super(name, textField);
-        this.scrollPane = new JScrollPane(super.get());
+        scrollPane = new JScrollPane(super.get(), JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setWheelScrollingEnabled(true);
+        JScrollBar horizontal = scrollPane.getHorizontalScrollBar();
+        JScrollBar vertical = scrollPane.getVerticalScrollBar();
+        Color background = Colors.isDark(textField.getBackground().getRGB()) ? textField.getBackground().brighter() : textField.getBackground().darker();
+        horizontal.setPreferredSize(new Dimension(10, 7));
+        horizontal.setBackground(background);
+        horizontal.setUI(new BasicScrollBarUI() {
+            @Override
+            protected void configureScrollBarColors() {
+                this.thumbColor =
+                        Colors.isDark(textField.getBackground().getRGB()) ?
+                                textField.getBackground().brighter().brighter() :
+                                textField.getBackground().darker().darker();
+            }
+
+
+        });
+        vertical.setPreferredSize(new Dimension(7, 10));
+        vertical.setBackground(background);
+        vertical.setUI(new BasicScrollBarUI() {
+            @Override
+            protected void configureScrollBarColors() {
+                this.thumbColor = Colors.isDark(textField.getBackground().getRGB()) ?
+                        textField.getBackground().brighter().brighter() :
+                        textField.getBackground().darker().darker();
+            }
+        });
     }
 
     @Override
