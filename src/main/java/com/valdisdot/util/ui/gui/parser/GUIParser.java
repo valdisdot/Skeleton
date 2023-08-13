763dd991583f531501741995e4bac563093554d2
valdisdot <valdisdot@gmail.com>
2023-08-13 23:20:09 +0300

    create interface GUIParser
    - implementations must provide the List of FrameMolds

---
+++ src/main/java/com/valdisdot/util/ui/gui/parser/GUIParser.java
@@ -0,0 +1,10 @@
+package com.valdisdot.util.ui.gui.parser;
+
+import com.valdisdot.util.ui.gui.parser.mold.FrameMold;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+//interface for future implementations
+public interface GUIParser extends Supplier<List<FrameMold>> {
+}
