fac347749c7b9153dcc9604b5b973b5e92670ffe
valdisdot <valdisdot@gmail.com>
2023-08-15 18:19:21 +0300

    JElement: change generic type from T to D (data)

--- src/main/java/com/valdisdot/util/ui/gui/element/JElement.java
+++ src/main/java/com/valdisdot/util/ui/gui/element/JElement.java
@@ -7,14 +7,14 @@ import javax.swing.*;
 import java.util.Objects;
 
 //common Element implementation for javax.swing
-public abstract class JElement<T> implements Element<T, JComponent> {
-    private DataCell<T> dataCell;
+public abstract class JElement<D> implements Element<D, JComponent> {
+    private DataCell<D> dataCell;
     private JComponent component;
 
     protected abstract boolean pleaseAcceptThatYouHaveDone();
 
     //cuz Constructor.super must be the first line
-    protected void completeInitialization(JComponent component, DataCell<T> dataCell) {
+    protected void completeInitialization(JComponent component, DataCell<D> dataCell) {
         this.component = Objects.requireNonNull(component, "JComponent is not initialised");
         Objects.requireNonNull(component.getName(), "Component name is null: " + component);
         this.dataCell = dataCell;
@@ -33,7 +33,7 @@ public abstract class JElement<T> implements Element<T, JComponent> {
     }
 
     @Override
-    public DataCell<T> getDataCell() {
+    public DataCell<D> getDataCell() {
         return Objects.requireNonNull(dataCell, "DataCell is not initialised");
     }
 }
