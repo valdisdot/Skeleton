f1d156a9b0d5b9996d02c17cd1011fe2226a2d1e
valdisdot <valdisdot@gmail.com>
2023-08-15 22:21:14 +0300

    package-info.java: delete

--- src/main/java/com/valdisdot/util/package-info.java
+++
@@ -1,6 +0,0 @@
-package com.valdisdot.util;
-
-/*
- * Just tired to learn Spring/minimal front
- * Simple utility classes for pet-projects
- * */
--- src/test/java/com/valdisdot/util/package-info.java
+++
@@ -1,7 +0,0 @@
-package com.valdisdot.util;
-
-/*
-* The package contains quick run points (main methods) and tests.
-* Some cases don't contain any JUnits, sorry. I got not happy to test old Swing stuff.
-* The package is a part of pet project Utilities.
-* */
