19dcf3256d9a0bdf21984a187f01560569cf0849
valdisdot <valdisdot@gmail.com>
2023-08-18 18:58:13 +0300

    MoldParser: add method 'parse'

--- src/main/java/com/valdisdot/util/ui/gui/parser/MoldParser.java
+++ src/main/java/com/valdisdot/util/ui/gui/parser/MoldParser.java
@@ -1,6 +1,7 @@
 package com.valdisdot.util.ui.gui.parser;
 
 import com.valdisdot.util.data.element.DataCellGroup;
+import com.valdisdot.util.ui.gui.mold.FrameMold;
 
 import javax.swing.*;
 import java.awt.event.ActionListener;
@@ -10,6 +11,8 @@ import java.util.function.Supplier;
 
 //contact for parsers
 public interface MoldParser<P extends JPanel> extends Supplier<P> {
+    void parse(FrameMold frameMold);
+
     DataCellGroup<String> getDataCellGroups();
 
     Map<String, Consumer<ActionListener>> getButtonsActionListenerConsumers();
