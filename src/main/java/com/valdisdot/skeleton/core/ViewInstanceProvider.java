962946959bdc486da183148a9a4ca877ef1a55d7
valdisdot <valdisdot@gmail.com>
2024-06-07 22:16:16 +0300

    create interface ViewInstanceProvider

---
+++ src/main/java/com/valdisdot/skeleton/core/ViewInstanceProvider.java
@@ -0,0 +1,27 @@
+package com.valdisdot.skeleton.core;
+
+import com.valdisdot.skeleton.util.PropertiesMap;
+
+import java.util.Optional;
+
+/**
+ * The interface represents a special factory object, which can be used for retrieving view instances and common properties for the application.
+ *
+ * @param <ContainerViewType> the view type of the container.
+ * @param <ComponentViewType> the view type of internal view components.
+ * @param <DataType>          type of data exchange between view components and business logic.
+ * @author Vladyslav Tymchenko
+ * @since 1.0
+ */
+public interface ViewInstanceProvider<ContainerViewType, ComponentViewType, DataType> {
+    /**
+     * @param id an id of the required ViewInstance.
+     * @return an Optional of ViewInstance from this provider with types {@code <ContainerViewType, ComponentViewType, DataType>}
+     */
+    Optional<ViewInstance<ContainerViewType, ComponentViewType, DataType>> getInstance(String id);
+
+    /**
+     * @return a properties with common values for the whole application.
+     */
+    PropertiesMap getProperties();
+}
