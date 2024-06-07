b0b462063f85305b75ef616f742d686616e0cf10
valdisdot <valdisdot@gmail.com>
2024-06-07 22:05:00 +0300

    add javadoc into the Identifiable

--- src/main/java/com/valdisdot/skeleton/core/Identifiable.java
+++ src/main/java/com/valdisdot/skeleton/core/Identifiable.java
@@ -1,8 +1,23 @@
 package com.valdisdot.skeleton.core;
 
+
+/**
+ * The mark interface of the identifiable element (with an ID).
+ *
+ * @author Vladyslav Tymchenko
+ * @apiNote All essential library's components are identifiable.
+ * @since 1.0
+ */
 public interface Identifiable extends Comparable<Identifiable> {
+    /**
+     * @return an identifier of the element.
+     */
     String getId();
 
+    /**
+     * @param other an Identifiable to be compared.
+     * @return a comparing integer of IDs.
+     */
     @Override
     default int compareTo(Identifiable other) {
         return getId().compareTo(other.getId());
