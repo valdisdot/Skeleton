3ed5c5f79cb5ca44c09caaa9ec1989e94107e2e5
valdis <valdisdot@gmail.com>
2024-06-11 11:12:01 +0300

    make javadoc for Functions

--- src/main/java/com/valdisdot/skeleton/util/Functions.java
+++ src/main/java/com/valdisdot/skeleton/util/Functions.java
@@ -4,14 +4,28 @@ import java.util.Collection;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+/**
+ * The util class which contains default converting functions.
+ */
 public class Functions {
     private Functions() {
     }
 
+    /**
+     * Collection to string function.
+     *
+     * @return the function
+     */
     public static Function<Collection<String>, String> collectionToString() {
         return Object::toString;
     }
 
+
+    /**
+     * Collection to json array function.
+     *
+     * @return the function
+     */
     public static Function<Collection<String>, String> collectionToJsonArray() {
         return collection -> collection.stream().collect(Collectors.joining("[", ", ", "]"));
     }
