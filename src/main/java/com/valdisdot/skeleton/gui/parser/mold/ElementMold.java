02631d0c8b1fafc7e25251501be8f6594867abf8
valdis <valdisdot@gmail.com>
2024-06-11 12:57:02 +0300

    make javadoc for ElementMold and modify it
    - set fields visibility to private
    - remove protected empty constructor

--- src/main/java/com/valdisdot/skeleton/gui/parser/mold/ElementMold.java
+++ src/main/java/com/valdisdot/skeleton/gui/parser/mold/ElementMold.java
@@ -4,51 +4,107 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
 
+/**{@inheritDoc}*/
 public class ElementMold extends Mold {
-    protected String type;
-    protected boolean doesFollow = false;
-    protected boolean isControl = false;
-    protected Map<String, String> values;
+    private String type;
+    private boolean doesFollow = false;
+    private boolean isControl = false;
+    private Map<String, String> values;
 
+    /**
+     * Instantiates a new Element mold. ID must not be null.
+     *
+     * @param id     the id
+     * @param type   the type
+     * @param values the values
+     */
     public ElementMold(String id, String type, Map<String, String> values) {
-        super(id);
+        super(Objects.requireNonNull(id, "ID for ElementMold is null"));
         this.type = Objects.requireNonNullElse(type, "");
         this.values = new LinkedHashMap<>(Objects.requireNonNullElse(values, Map.of()));
     }
 
-    //open for inheritance
-    protected ElementMold() {
-        super();
+    /**
+     * Instantiates a new Element mold. ID must not be null.
+     *
+     * @param id   the id
+     * @param type the type
+     */
+    public ElementMold(String id, String type){
+        this(id, type, null);
     }
 
+    /**
+     * Instantiates a new Element mold.
+     *
+     * @param id the id
+     */
+    public ElementMold(String id){
+        this(id, null, null);
+    }
+
+    /**
+     * Return a text representation of type of this element mold.
+     *
+     * @return the type
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Sets a text representation of type of this element mold.
+     *
+     * @param type the type
+     */
     public void setType(String type) {
         this.type = Objects.requireNonNullElse(type, this.type);
     }
 
+    /**
+     * @return the flag indicates the requirement of leading previous element after the previous.
+     */
     public boolean doesFollow() {
         return doesFollow;
     }
 
+    /**
+     * Sets the flag about requirement of leading previous element after the previous.
+     *
+     * @param value the value
+     */
     public void doFollow(boolean value) {
         this.doesFollow = value;
     }
 
+    /**
+     * @return the flag indicates the requirement to treat the current element as a control element.
+     */
     public boolean isControl() {
         return isControl;
     }
 
+    /**
+     * Sets the flag indicates the requirement to treat the current element as a control element.
+     *
+     * @param isControl the value
+     */
     public void setControl(boolean isControl) {
         this.isControl = isControl;
     }
 
+    /**
+     * Returns the values of this element.
+     * @return the values
+     */
     public Map<String, String> getValues() {
         return values;
     }
 
+    /**
+     * Sets the values of this element.
+     * @param values the values
+     */
     public void setValues(Map<String, String> values) {
         if (Objects.nonNull(values)) this.values.putAll(values);
     }
