4193293f4e039f55b76f7f4d40a3a7fa9bf1b3ca
valdis <valdisdot@gmail.com>
2024-06-11 12:57:42 +0300

    make javadoc for PanelMold and modify it
    - set fields visibility to private
    - remove protected empty constructor

--- src/main/java/com/valdisdot/skeleton/gui/parser/mold/PanelMold.java
+++ src/main/java/com/valdisdot/skeleton/gui/parser/mold/PanelMold.java
@@ -2,36 +2,65 @@ package com.valdisdot.skeleton.gui.parser.mold;
 
 import java.util.*;
 
+/**
+ * {@inheritDoc}
+ */
 public class PanelMold extends Mold {
-    protected List<ElementMold> elements;
+    private List<ElementMold> elements;
     private Scope scope;
 
+    /**
+     * Instantiates a new Panel mold. ID must not be null.
+     *
+     * @param id    the id
+     * @param scope the scope
+     */
     public PanelMold(String id, Scope scope) {
-        super(id);
+        super(Objects.requireNonNull(id, "ID for PanelMold is null"));
         this.elements = new LinkedList<>();
         this.scope = Objects.requireNonNullElse(scope, Scope.SINGLETON);
     }
 
+    /**
+     * Instantiates a new Panel mold. ID must not be null.
+     *
+     * @param id the id
+     */
     public PanelMold(String id) {
         this(id, null);
     }
 
-    protected PanelMold() {
-        super();
-    }
-
+    /**
+     * Adds an element mold to this panel mold.
+     *
+     * @param elementMold the element mold
+     */
     public void addElement(ElementMold elementMold) {
         elements.add(elementMold);
     }
 
+    /**
+     * Adds a collection of element molds to this panel mold.
+     *
+     * @param elementMolds the element molds
+     */
     public void addElements(Collection<ElementMold> elementMolds) {
         elements.addAll(elementMolds);
     }
 
+    /**
+     * @return a collection of element molds from this panel mold.
+     */
     public List<ElementMold> getElements() {
         return List.copyOf(elements);
     }
 
+    /**
+     * Returns a list of lists of element molds.
+     * Each list element can be treated as a row/column.
+     *
+     * @return the elements ordered
+     */
     public List<List<ElementMold>> getElementsOrdered() {
         List<List<ElementMold>> res = new LinkedList<>();
         if (!elements.isEmpty()) {
@@ -55,6 +84,14 @@ public class PanelMold extends Mold {
         return res;
     }
 
+    /**
+     * @return the scope of this panel mold
+     * @see Scope
+     */
+    public Scope getScope() {
+        return scope;
+    }
+
     @Override
     public String toString() {
         return "PanelMold{" +
@@ -66,11 +103,12 @@ public class PanelMold extends Mold {
                 '}';
     }
 
-    public Scope getScope() {
-        return scope;
-    }
-
+    /**
+     * The enum Scope. Indicates an instantiation type of this PanelMold.
+     * Can be either prototype or singleton (aka Spring DI)
+     */
     public enum Scope {
-        PROTOTYPE, SINGLETON
+        PROTOTYPE,
+        SINGLETON
     }
 }
