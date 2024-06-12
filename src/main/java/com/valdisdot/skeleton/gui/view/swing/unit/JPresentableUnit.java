90314081c6fc36472ba6f958b252a60cc4140e10
valdis <valdisdot@gmail.com>
2024-06-12 11:18:38 +0300

    make javadoc for JPresentableUnit

--- src/main/java/com/valdisdot/skeleton/gui/view/swing/unit/JPresentableUnit.java
+++ src/main/java/com/valdisdot/skeleton/gui/view/swing/unit/JPresentableUnit.java
@@ -6,6 +6,11 @@ import com.valdisdot.skeleton.gui.view.swing.JElement;
 import javax.swing.*;
 import java.util.function.Consumer;
 
+/**
+ * The helper abstract parent class for code reducing. Contains common for all Swing implementations methods and default implementations of some PresentableUnit methods.
+ * @since 1.0
+ * @author Vladyslav Tymchenko
+ */
 public abstract class JPresentableUnit extends JElement implements PresentableUnit<JComponent> {
     protected JPresentableUnit(JComponent component) {
         super(component);
@@ -16,6 +21,7 @@ public abstract class JPresentableUnit extends JElement implements PresentableUn
         super();
     }
 
+    /**{@inheritDoc}*/
     @Override
     public void apply(Consumer<JComponent> method) {
         method.accept(getComponent());
