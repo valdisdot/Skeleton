2710b5940e7326299ea249e869739ce6de82b772
valdis <valdisdot@gmail.com>
2024-06-12 11:18:49 +0300

    make javadoc for JSinglePresentableUnit

--- src/main/java/com/valdisdot/skeleton/gui/view/swing/unit/JSinglePresentableUnit.java
+++ src/main/java/com/valdisdot/skeleton/gui/view/swing/unit/JSinglePresentableUnit.java
@@ -5,6 +5,11 @@ import com.valdisdot.skeleton.core.view.PresentablePair;
 import javax.swing.*;
 import java.util.Collection;
 
+/**
+ * The abstract implementation for elements where a view presentation is singular and does not contain inner elements (like label, button etc.).
+ * @since 1.0
+ * @author Vladyslav Tymchenko
+ */
 public abstract class JSinglePresentableUnit extends JPresentableUnit {
     protected JSinglePresentableUnit() {
     }
@@ -13,16 +18,19 @@ public abstract class JSinglePresentableUnit extends JPresentableUnit {
         super(component);
     }
 
+    /**{@inheritDoc}*/
     @Override
     public void updatePresentations(Collection<PresentablePair> presentations) {
         presentations.stream().findFirst().ifPresent(entry -> setPresentation(entry.toString()));
     }
 
+    /**{@inheritDoc}*/
     @Override
     public void completePresentations(Collection<PresentablePair> presentations) {
         presentations.stream().findFirst().ifPresent(entry -> setPresentation(entry.toString()));
     }
 
+    /**{@inheritDoc}*/
     @Override
     public void replacePresentations(Collection<PresentablePair> presentations) {
         presentations.stream().findFirst().ifPresent(entry -> setPresentation(entry.toString()));
