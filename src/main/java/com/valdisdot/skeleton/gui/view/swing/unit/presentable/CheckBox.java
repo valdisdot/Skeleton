8e7b016e108107731ce176b12c083a98ea7dab61
valdisdot <valdisdot@gmail.com>
2024-06-14 12:48:26 +0300

    update CheckBox
    - make javadoc
    - add if-empty validation in 'presentation' methods

--- src/main/java/com/valdisdot/skeleton/gui/view/swing/unit/presentable/CheckBox.java
+++ src/main/java/com/valdisdot/skeleton/gui/view/swing/unit/presentable/CheckBox.java
@@ -7,12 +7,26 @@ import com.valdisdot.skeleton.gui.view.swing.unit.JSinglePresentableUnit;
 import javax.swing.*;
 import java.util.Objects;
 
+/**
+ * The implementation of a checkbox element.
+ * @since 1.0
+ * @author Vladyslav Tymchenko
+ */
 public class CheckBox extends JSinglePresentableUnit implements DataUnit<String> {
     private final boolean preselected;
     private final String selected;
     private final String deselected;
     private final JCheckBox checkBox;
 
+    /**
+     * Instantiates a new checkbox.
+     *
+     * @param id          an id, not null
+     * @param title       a title, nullable
+     * @param preselected flag indicates an initial state of the checkbox
+     * @param selected    a selected value, nullable
+     * @param deselected  a deselected value, nullable
+     */
     public CheckBox(String id, String title, boolean preselected, String selected, String deselected) {
         JCheckBox checkBox = new JCheckBox(title == null ? "" : title);
         checkBox.setSelected(preselected);
@@ -23,23 +37,44 @@ public class CheckBox extends JSinglePresentableUnit implements DataUnit<String>
         this.checkBox = setComponent(checkBox);
     }
 
+    /**
+     * Instantiates a new checkbox.
+     *
+     * @param id         an id, not null
+     * @param title      a title, nullable
+     * @param selected   a selected value, nullable
+     * @param deselected a deselected value, nullable
+     */
     public CheckBox(String id, String title, String selected, String deselected) {
         this(id, title, false, selected, deselected);
     }
 
+    /**
+     * Instantiates a new checkbox.
+     *
+     * @param id    an id, not null
+     * @param title a title
+     */
     public CheckBox(String id, String title) {
         this(id, title, false, null, null);
     }
 
+    /**
+     * Instantiates a new checkbox.
+     *
+     * @param id an id
+     */
     public CheckBox(String id) {
         this(id, null, false, null, null);
     }
 
+    /**{@inheritDoc}*/
     @Override
     public DataBean<String> getBean() {
         return new DataBean<>(getId(), checkBox.isSelected() ? selected : deselected);
     }
 
+    /**{@inheritDoc}*/
     @Override
     public void setBean(DataBean<String> data) {
         if (data.isPresent()) {
@@ -52,13 +87,15 @@ public class CheckBox extends JSinglePresentableUnit implements DataUnit<String>
         //else do nothing
     }
 
+    /**{@inheritDoc}*/
     @Override
     public void reset() {
         checkBox.setSelected(preselected);
     }
 
+    /**{@inheritDoc}*/
     @Override
     public void setPresentation(String presentation) {
-        checkBox.setText(presentation);
+        if(presentation != null) checkBox.setText(presentation);
     }
 }
