080aa9bec50550f982f27884a764ed2a2acdaad4
valdisdot <valdisdot@gmail.com>
2024-06-14 12:50:48 +0300

    update Slider
    - make javadoc
    - add force remove-all in the 'updateView()'

--- src/main/java/com/valdisdot/skeleton/gui/view/swing/unit/presentable/Slider.java
+++ src/main/java/com/valdisdot/skeleton/gui/view/swing/unit/presentable/Slider.java
@@ -12,9 +12,22 @@ import java.util.Hashtable;
 import java.util.List;
 import java.util.function.Consumer;
 
+/**
+ * The implementation of a slider element.
+ * @since 1.0
+ * @author Vladyslav Tymchenko
+ */
 public class Slider extends JMultiPresentableUnit implements DataUnit<String> {
     private final JSlider slider;
 
+    /**
+     * Instantiates a new slider.
+     *
+     * @param id         an id, not null
+     * @param view       a view, nullable
+     * @param isVertical flag indicates an orientation of the slider
+     * @see PresentablePair
+     */
     public Slider(String id, Collection<PresentablePair> view, boolean isVertical) {
         JSlider slider = new JSlider(isVertical ? JSlider.VERTICAL : JSlider.HORIZONTAL);
         slider.setName(id);
@@ -24,19 +37,33 @@ public class Slider extends JMultiPresentableUnit implements DataUnit<String> {
         replacePresentations(view);
     }
 
+    /**
+     * Instantiates a new slider.
+     *
+     * @param id   an id, not null
+     * @param view a view, nullable
+     * @see PresentablePair
+     */
     public Slider(String id, Collection<PresentablePair> view) {
         this(id, view, false);
     }
 
+    /**
+     * Instantiates a new slider.
+     *
+     * @param id an id, not null
+     */
     public Slider(String id) {
         this(id, null, false);
     }
 
+    /**{@inheritDoc}*/
     @Override
     public DataBean<String> getBean() {
         return new DataBean<>(getId(), getCurrentView().get(slider.getValue()).toString());
     }
 
+    /**{@inheritDoc}*/
     @Override
     public void setBean(DataBean<String> data) {
         if (data.isPresent()) {
@@ -51,17 +78,20 @@ public class Slider extends JMultiPresentableUnit implements DataUnit<String> {
         }
     }
 
+    /**{@inheritDoc}*/
     @Override
     public void reset() {
         slider.setValue(0);
     }
 
+    /**{@inheritDoc}*/
     @Override
     public void apply(Consumer<JComponent> method) {
         super.apply(method);
         updateView();
     }
 
+    /**{@inheritDoc}*/
     @Override
     public Object applyStyle(Style style) {
         try {
@@ -71,8 +101,10 @@ public class Slider extends JMultiPresentableUnit implements DataUnit<String> {
         }
     }
 
+    /**{@inheritDoc}*/
     @Override
     protected void updateView() {
+        slider.setLabelTable(null);
         List<PresentablePair> view = getCurrentView();
         if (!view.isEmpty()) {
             int index = 0;
@@ -86,7 +118,6 @@ public class Slider extends JMultiPresentableUnit implements DataUnit<String> {
             }
             slider.setMaximum(0);
             slider.setMaximum(view.size() - 1);
-            slider.setLabelTable(null);
             slider.setLabelTable(labels);
         }
     }
