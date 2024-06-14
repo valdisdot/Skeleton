2e15074771fe456194483620ee7496b8ccfe7a10
valdisdot <valdisdot@gmail.com>
2024-06-14 12:47:23 +0300

    update Label
    - make javadoc
    - add if-empty validation in 'presentation' methods

--- src/main/java/com/valdisdot/skeleton/gui/view/swing/unit/presentable/Label.java
+++ src/main/java/com/valdisdot/skeleton/gui/view/swing/unit/presentable/Label.java
@@ -6,10 +6,21 @@ import com.valdisdot.skeleton.gui.view.swing.unit.JSinglePresentableUnit;
 
 import javax.swing.*;
 
+/**
+ * The implementation of a label element.
+ * @since 1.0
+ * @author Vladyslav Tymchenko
+ */
 public class Label extends JSinglePresentableUnit implements DataUnit<String> {
     private final JLabel label;
     private String title;
 
+    /**
+     * Instantiates a new label.
+     *
+     * @param id    an id, not null
+     * @param title a title, nullable
+     */
     public Label(String id, String title) {
         this.title = title == null ? "" : title;
         JLabel label = new JLabel(this.title);
@@ -17,28 +28,39 @@ public class Label extends JSinglePresentableUnit implements DataUnit<String> {
         this.label = setComponent(label);
     }
 
+    /**
+     * Instantiates a new label.
+     *
+     * @param id an id, not null
+     */
     public Label(String id) {
         this(id, null);
     }
 
+    /**{@inheritDoc}*/
     @Override
     public DataBean<String> getBean() {
         return new DataBean<>(getId(), label.getText());
     }
 
+    /**{@inheritDoc}*/
     @Override
     public void setBean(DataBean<String> data) {
         if (data.isPresent()) label.setText(data.fetchFirst());
     }
 
+    /**{@inheritDoc}*/
     @Override
     public void reset() {
         label.setText(title);
     }
 
+    /**{@inheritDoc}*/
     @Override
     public void setPresentation(String presentation) {
-        title = presentation;
-        label.setText(title);
+        if(presentation != null) {
+            title = presentation;
+            label.setText(title);
+        }
     }
 }
