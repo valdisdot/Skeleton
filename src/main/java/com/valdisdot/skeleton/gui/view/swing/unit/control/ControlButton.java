3ec4a8f68d1046dfe55112f4bd03e88f65ac6b8e
valdis <valdisdot@gmail.com>
2024-06-24 11:20:52 +0300

    add an option to use the ExecutorService for click handlers in the ControlButton

--- src/main/java/com/valdisdot/skeleton/gui/view/swing/unit/control/ControlButton.java
+++ src/main/java/com/valdisdot/skeleton/gui/view/swing/unit/control/ControlButton.java
@@ -9,16 +9,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.Collection;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 import java.util.function.Consumer;
 
 /**
  * The implementation of ControlUnit for clickable button.
- * @apiNote Pay attention that each user action will produce a new Thread.
+ * @apiNote Pay attention that each user action will produce a new Thread if no ExecutorService was provided.
  * @since 1.0
  * @author Vladyslav Tymchenko
  */
 public class ControlButton extends JSinglePresentableUnit implements ControlUnit<JComponent> {
     private final JButton button;
+    private ExecutorService executorService = null;
 
     /**
      * Instantiates a new button.
@@ -43,47 +46,86 @@ public class ControlButton extends JSinglePresentableUnit implements ControlUnit
         this(id, null);
     }
 
+    public void setExecutorService(ExecutorService executorService){
+        this.executorService = executorService;
+    }
+
     /**{@inheritDoc}*/
     @Override
     public <Data> void addDataActionFor(DataUnit<Data> element, Consumer<DataUnit<Data>> withMethod) {
-        button.addActionListener(l -> new Thread(() -> withMethod.accept(element)).start());
+        button.addActionListener(
+                executorService == null ?
+                l -> new Thread(() -> withMethod.accept(element)).start() :
+                l -> executorService.execute(() -> withMethod.accept(element))
+                );
     }
 
     /**{@inheritDoc}*/
     @Override
     public <Data> void addDataActionFor(Collection<DataUnit<Data>> elements, Consumer<DataUnit<Data>> withMethod) {
-        button.addActionListener(l -> new Thread(() -> elements.forEach(withMethod)).start());
+        button.addActionListener(
+                executorService == null ?
+                l -> new Thread(() -> elements.forEach(withMethod)).start() :
+                l -> executorService.execute(() -> elements.forEach(withMethod))
+        );
     }
 
     /**{@inheritDoc}*/
     @Override
     public void addAction(Runnable action) {
-        button.addActionListener(l -> new Thread(action).start());
+        button.addActionListener(
+                executorService == null ?
+                l -> new Thread(action).start() :
+                l -> executorService.execute(action));
     }
 
     /**{@inheritDoc}*/
     @Override
     public <Data> void addAction(Callable<Data> callable, Consumer<Data> resultConsumer, Consumer<Exception> exceptionConsumer) {
         button.addActionListener(
-                l -> new Thread(() -> {
-                    try {
-                        resultConsumer.accept(callable.call());
-                    } catch (Exception e) {
-                        exceptionConsumer.accept(e);
+                    executorService == null ?
+                    l -> new Thread(() -> {
+                        try {
+                            resultConsumer.accept(callable.call());
+                        } catch (Exception e) {
+                            exceptionConsumer.accept(e);
+                        }
+                    }).start() :
+                    l -> {
+                        try {
+                            Future<Data> resultFuture = executorService.submit(callable);
+                            executorService.execute(() -> {
+                                try {
+                                    resultConsumer.accept(resultFuture.get());
+                                } catch (Exception e) {
+                                    exceptionConsumer.accept(e);
+                                }
+                            });
+                        } catch (Exception e) {
+                            exceptionConsumer.accept(e);
+                        }
                     }
-                }).start());
+                );
     }
 
     /**{@inheritDoc}*/
     @Override
     public <AView> void addViewActionFor(PresentableUnit<AView> element, Consumer<PresentableUnit<AView>> withMethod) {
-        button.addActionListener(l -> new Thread(() -> withMethod.accept(element)).start());
+        button.addActionListener(
+                executorService == null ?
+                l -> new Thread(() -> withMethod.accept(element)).start() :
+                l -> executorService.execute(() -> withMethod.accept(element))
+        );
     }
 
     /**{@inheritDoc}*/
     @Override
     public <AView> void addViewActionFor(Collection<PresentableUnit<AView>> elements, Consumer<PresentableUnit<AView>> withMethod) {
-        button.addActionListener(l -> new Thread(() -> elements.forEach(withMethod)).start());
+        button.addActionListener(
+                executorService == null ?
+                l -> new Thread(() -> elements.forEach(withMethod)).start() :
+                l -> executorService.execute(() -> elements.forEach(withMethod))
+                );
     }
 
     /**{@inheritDoc}*/
