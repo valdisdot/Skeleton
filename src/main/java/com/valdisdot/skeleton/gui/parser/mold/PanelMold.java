19af8af8bdb2c598b26ee69005af7732cdcf4141
valdis <valdisdot@gmail.com>
2024-10-02 13:31:58 +0300

    update class PanelMold
    - add method clone()
    - update JavaDoc

--- src/main/java/com/valdisdot/skeleton/gui/parser/mold/PanelMold.java
+++ src/main/java/com/valdisdot/skeleton/gui/parser/mold/PanelMold.java
@@ -3,8 +3,20 @@ package com.valdisdot.skeleton.gui.parser.mold;
 import java.util.*;
 
 /**
- * {@inheritDoc}
+ * Represents a panel mold that can contain a collection of {@link ElementMold} elements and
+ * has a defined {@link Scope} for managing its lifecycle.
+ * <p>
+ * A panel mold serves as a container for other element molds, and the elements can be
+ * added, removed, or ordered within the panel.
+ * </p>
+ * <p>
+ * The {@code PanelMold} has an {@code id} that must be provided during instantiation and
+ * an optional {@link Scope} which defines its instantiation type (singleton or prototype).
+ * </p>
+ *
  * @since 1.0
+ * @see ElementMold
+ * @see Scope
  * @author Vladyslav Tymchenko
  */
 public class PanelMold extends Mold {
@@ -12,10 +24,10 @@ public class PanelMold extends Mold {
     private Scope scope;
 
     /**
-     * Instantiates a new Panel mold. ID must not be null.
+     * Instantiates a new Panel mold. The {@code id} must not be {@code null}.
      *
-     * @param id    the id
-     * @param scope the scope
+     * @param id    the unique identifier for this panel mold
+     * @param scope the scope defining the lifecycle of the panel mold (optional, defaults to {@link Scope#SINGLETON})
      */
     public PanelMold(String id, Scope scope) {
         super(Objects.requireNonNull(id, "ID for PanelMold is null"));
@@ -24,85 +36,136 @@ public class PanelMold extends Mold {
     }
 
     /**
-     * Instantiates a new Panel mold. ID must not be null.
+     * Instantiates a new Panel mold with the default {@link Scope#SINGLETON}.
+     * The {@code id} must not be {@code null}.
      *
-     * @param id the id
+     * @param id the unique identifier for this panel mold
      */
     public PanelMold(String id) {
         this(id, null);
     }
 
     /**
-     * Adds an element mold to this panel mold.
+     * Adds an {@link ElementMold} to this panel mold.
      *
-     * @param elementMold the element mold
+     * @param elementMold the element mold to add
      */
     public void addElement(ElementMold elementMold) {
         elements.add(elementMold);
     }
 
     /**
-     * Adds a collection of element molds to this panel mold.
+     * Adds a collection of {@link ElementMold}s to this panel mold.
      *
-     * @param elementMolds the element molds
+     * @param elementMolds the element molds to add
      */
     public void addElements(Collection<ElementMold> elementMolds) {
         elements.addAll(elementMolds);
     }
 
+    /**
+     * Inserts an {@link ElementMold} at the specified index.
+     * If the index is out of bounds, the element will be added at the end.
+     *
+     * @param elementMold the element mold to insert
+     * @param atIndex     the index at which to insert the element
+     */
     public void insertElement(ElementMold elementMold, int atIndex) {
-        try{
+        try {
             elements.add(atIndex, elementMold);
         } catch (IndexOutOfBoundsException e) {
             elements.add(elementMold);
         }
     }
 
+    /**
+     * Inserts an {@link ElementMold} after the element mold with the specified ID.
+     *
+     * @param elementMold        the element mold to insert
+     * @param afterElementMoldId the ID of the element mold after which to insert
+     */
     public void insertElement(ElementMold elementMold, String afterElementMoldId) {
         insertElement(elementMold, findIndex(afterElementMoldId, true, 1));
     }
 
+    /**
+     * Inserts a collection of {@link ElementMold}s at the specified index.
+     * If the index is out of bounds, the elements will be added at the end.
+     *
+     * @param elementMolds the collection of element molds to insert
+     * @param atIndex      the index at which to insert the elements
+     */
     public void insertElements(Collection<ElementMold> elementMolds, int atIndex) {
         try {
             elements.addAll(atIndex, elementMolds);
-        }  catch (IndexOutOfBoundsException e) {
+        } catch (IndexOutOfBoundsException e) {
             elements.addAll(elementMolds);
         }
     }
 
+    /**
+     * Inserts a collection of {@link ElementMold}s after the element mold with the specified ID.
+     *
+     * @param elementMolds       the collection of element molds to insert
+     * @param afterElementMoldId the ID of the element mold after which to insert the collection
+     */
     public void insertElements(Collection<ElementMold> elementMolds, String afterElementMoldId) {
         insertElements(elementMolds, findIndex(afterElementMoldId, true, 1));
     }
 
+    /**
+     * Replaces the element at the specified index with a new {@link ElementMold}.
+     * If the index is out of bounds, the new element is added.
+     *
+     * @param newElement the new element mold to replace
+     * @param atIndex    the index at which to replace the element
+     */
     public void replaceElement(ElementMold newElement, int atIndex) {
-        if(atIndex >= 0 && atIndex < elements.size()) elements.set(atIndex, newElement);
+        if (atIndex >= 0 && atIndex < elements.size()) elements.set(atIndex, newElement);
         else elements.add(newElement);
     }
 
+    /**
+     * Replaces the element with the specified ID with a new {@link ElementMold}.
+     *
+     * @param newElement          the new element mold to replace
+     * @param targetElementMoldId the ID of the element mold to be replaced
+     */
     public void replaceElement(ElementMold newElement, String targetElementMoldId) {
         replaceElement(newElement, findIndex(targetElementMoldId, false, 0));
     }
 
+    /**
+     * Removes the element at the specified index.
+     *
+     * @param atIndex the index of the element to remove
+     */
     public void removeElement(int atIndex) {
-        if(atIndex >= 0 && atIndex < elements.size()) elements.remove(atIndex);
+        if (atIndex >= 0 && atIndex < elements.size()) elements.remove(atIndex);
     }
 
+    /**
+     * Removes the element with the specified ID.
+     *
+     * @param targetElementMoldId the ID of the element mold to remove
+     */
     public void removeElement(String targetElementMoldId) {
         removeElement(findIndex(targetElementMoldId, false, 0));
     }
 
     /**
-     * @return a collection of element molds from this panel mold.
+     * Returns a list of all element molds in this panel mold.
+     *
+     * @return an unmodifiable list of element molds
      */
     public List<ElementMold> getElements() {
         return List.copyOf(elements);
     }
 
     /**
-     * Returns a list of lists of element molds.
-     * Each list element can be treated as a row/column.
+     * Returns a list of lists of element molds, where each sublist can be treated as a row or column.
      *
-     * @return the elements ordered
+     * @return a list of ordered elements
      */
     public List<List<ElementMold>> getElementsOrdered() {
         List<List<ElementMold>> res = new LinkedList<>();
@@ -128,6 +191,8 @@ public class PanelMold extends Mold {
     }
 
     /**
+     * Returns the {@link Scope} of this panel mold.
+     *
      * @return the scope of this panel mold
      * @see Scope
      */
@@ -135,10 +200,29 @@ public class PanelMold extends Mold {
         return scope;
     }
 
+    /**
+     * Clones this panel mold, creating a new {@code PanelMold} with the same ID, scope, and elements.
+     *
+     * @return the cloned panel mold
+     */
+    public PanelMold clone() {
+        PanelMold panelMold = new PanelMold(this.id, this.scope);
+        panelMold.elements.addAll(this.elements);
+        return panelMold;
+    }
+
+    /**
+     * Finds the index of an element mold by its ID.
+     *
+     * @param elementMoldId  the ID of the element mold
+     * @param excludeLast    whether to exclude the last element from the search
+     * @param shift          an offset applied to the found index
+     * @return the index of the element mold, or -1 if not found
+     */
     private int findIndex(String elementMoldId, boolean excludeLast, int shift) {
         Iterator<ElementMold> iterator = elements.iterator();
-        for(int i = 0; i < (elements.size() - (excludeLast ? 1 : 0)); ++i){
-            if(elementMoldId.equals(iterator.next().getId())) {
+        for (int i = 0; i < (elements.size() - (excludeLast ? 1 : 0)); ++i) {
+            if (elementMoldId.equals(iterator.next().getId())) {
                 return i + shift;
             }
         }
@@ -157,8 +241,11 @@ public class PanelMold extends Mold {
     }
 
     /**
-     * The enum Scope. Indicates an instantiation type of this PanelMold.
-     * Can be either prototype or singleton (aka Spring DI)
+     * The {@code Scope} enum defines the instantiation type of this {@code PanelMold}.
+     * <p>
+     * Can either be {@code PROTOTYPE}, indicating a new instance is created each time, or
+     * {@code SINGLETON}, indicating the same instance is reused.
+     * </p>
      */
     public enum Scope {
         PROTOTYPE,
