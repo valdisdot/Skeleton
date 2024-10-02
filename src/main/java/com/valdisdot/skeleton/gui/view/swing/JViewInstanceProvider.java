b6727ab70a3a37853982709d011b606046ea9dcd
valdisdot <valdisdot@gmail.com>
2024-10-02 20:12:23 +0300

    implement changes in the ViewInstanceProvider

--- src/main/java/com/valdisdot/skeleton/gui/view/swing/JViewInstanceProvider.java
+++ src/main/java/com/valdisdot/skeleton/gui/view/swing/JViewInstanceProvider.java
@@ -10,8 +10,23 @@ import javax.swing.*;
 import java.util.*;
 
 /**
- * The class represents the implementation for Molds of a special factory object, which can be used for retrieving view instances and common properties for the application.
+ * The {@code JViewInstanceProvider} class represents an implementation of the {@link ViewInstanceProvider} interface
+ * for Swing-based applications, specifically handling {@link PanelMold} instances.
+ * <p>
+ * This class provides a factory object that retrieves view instances and common application properties.
+ * It supports both precompiled (singleton) and dynamically created (prototype) views.
+ * </p>
+ *
  * @since 1.0
+ * @see ViewInstanceProvider
+ * @see PanelMold
+ * @see MoldProvider
+ * @see PropertiesMap
+ * @see JViewInstance
+ * @see JPanel
+ * @see JComponent
+ * @see String
+ *
  * @author Vladyslav Tymchenko
  */
 public class JViewInstanceProvider implements ViewInstanceProvider<JPanel, JComponent, String> {
@@ -21,9 +36,13 @@ public class JViewInstanceProvider implements ViewInstanceProvider<JPanel, JComp
     private final PropertiesMap propertiesMap;
 
     /**
-     * Instantiates a new view instance provider.
+     * Instantiates a new {@code JViewInstanceProvider} using a {@link MoldProvider}.
+     * <p>
+     * The provider is used to gather precompiled singleton views and prototype molds.
+     * The properties from the provider are copied into this instance's {@link PropertiesMap}.
+     * </p>
      *
-     * @param provider the provider
+     * @param provider the {@link MoldProvider} containing {@link PanelMold} instances and properties.
      */
     public JViewInstanceProvider(MoldProvider provider) {
         this.id = provider.getId();
@@ -40,15 +59,26 @@ public class JViewInstanceProvider implements ViewInstanceProvider<JPanel, JComp
         propertiesMap.putAll(provider.getProperties());
     }
 
-    /**{@inheritDoc}*/
-    public Collection<String> getInstanceIds(){
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Returns a collection of all available instance IDs, which include both compiled and molded views.
+     * </p>
+     */
+    @Override
+    public Collection<String> getInstanceIds() {
         Set<String> keySet = new HashSet<>();
         keySet.addAll(compiledViews.keySet());
         keySet.addAll(moldedViews.keySet());
         return keySet;
     }
 
-    /**{@inheritDoc}*/
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Returns the ID of this {@code JViewInstanceProvider}.
+     * </p>
+     */
     @Override
     public String getId() {
         return id;
@@ -57,17 +87,48 @@ public class JViewInstanceProvider implements ViewInstanceProvider<JPanel, JComp
     /**
      * {@inheritDoc}
      * <p>
-     *     @apiNote Note that if the scope of the defined mold is {@code PanelMold.Scope.SINGLETON}, the method will return a precompiled it the construction JView if present. Otherwise, the method will construct a new JView each time the method called.
+     * If the {@link PanelMold} associated with the given ID is defined as {@code PanelMold.Scope.SINGLETON},
+     * a precompiled {@link JViewInstance} is returned. Otherwise, a new instance is constructed each time
+     * the method is called if the mold is of {@code PROTOTYPE} scope.
      * </p>
+     *
+     * @apiNote If the {@code PanelMold} has a scope of {@code SINGLETON}, the method will return a precompiled {@link JViewInstance}.
+     *          Otherwise, it constructs a new instance for every call.
+     *
+     * @param id the ID of the desired view instance.
+     * @return an {@link Optional} containing the view instance if found, or empty if not.
      */
     @Override
-    public Optional<ViewInstance<JPanel, JComponent, String>> getInstance(String id) {
+    public Optional<ViewInstance<JPanel, JComponent, String>> getInstanceById(String id) {
         return compiledViews.containsKey(id) ? Optional.ofNullable(compiledViews.get(id)) :
                 moldedViews.containsKey(id) ? Optional.of(new JViewInstance(moldedViews.get(id))) :
                         Optional.empty();
     }
 
-    /**{@inheritDoc}*/
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Retrieves a list of view instances whose IDs contain the provided token.
+     * This method searches both compiled and molded views.
+     * </p>
+     *
+     * @param idToken a string token to search for in instance IDs.
+     * @return a list of view instances whose IDs match the token.
+     */
+    @Override
+    public List<ViewInstance<JPanel, JComponent, String>> getInstancesByIdsLike(String idToken) {
+        List<ViewInstance<JPanel, JComponent, String>> res = new LinkedList<>();
+        for(String key : compiledViews.keySet()) if(key.contains(idToken)) res.add(compiledViews.get(key));
+        for(String key : moldedViews.keySet()) if(key.contains(idToken)) res.add(new JViewInstance(moldedViews.get(key)));
+        return res;
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Returns the {@link PropertiesMap} containing shared properties for the application.
+     * </p>
+     */
     @Override
     public PropertiesMap getProperties() {
         return propertiesMap;
